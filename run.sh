63fd5aa4aff619dce54ec80359978c9222b5a8d4
Paulo <paulo@hedra.com.br>
2023-05-03 15:19:32 -0300

    sh para rodar tudo

---
+++ run.sh
@@ -0,0 +1,39 @@
+cd 1ANO_MATEMATICA/
+make
+cd../
+
+cd 1ANO_PORTUGUES/
+make
+cd../
+
+cd 2ANO_MATEMATICA/
+make
+cd../
+
+cd 2ANO_PORTUGUES/
+make
+cd../
+
+cd 3ANO_MATEMATICA/
+make
+cd../
+
+cd 3ANO_PORTUGUES/
+make
+cd../
+
+cd 4ANO_MATEMATICA/
+make
+cd../
+
+cd 4ANO_PORTUGUES/
+make
+cd../
+
+cd 5ANO_MATEMATICA/
+make
+cd../
+
+cd 5ANO_PORTUGUES/
+make
+cd../
