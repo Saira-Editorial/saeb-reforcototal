dca0fe474ab864ae268eda8f16bb08d4c31df34a
Jorge Sallum <jorge@hedra.com.br>
2023-06-09 14:00:15 -0300

    apagando pastas inuteis

--- originais/run.sh
+++
@@ -1,3 +0,0 @@
-pandoc --extract-media=./_SAEB_9_MAT SAEB_9\ ANO_MAT.docx -o _SAEB_9_MAT.md
-pandoc --extract-media=./_SAEB_9_POR SAEB_9\ ANO_POR.docx -o _SAEB_9_POR.md
-pandoc --extract-media=./_SAEB_9_Sim SAEB_9\ ANO_Simulados.docx -o _SAEB_9_Sim.md
